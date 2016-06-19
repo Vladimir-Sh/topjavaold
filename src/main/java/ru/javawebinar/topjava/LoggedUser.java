049b1c013d7fc4a25014a48e09973fb40f15665b
Vladimir-Sh <my.vladimir@mail.ru>
2016-06-20 01:24:49 +0300

    HW2 partly optional2

--- src/main/java/ru/javawebinar/topjava/LoggedUser.java
+++ src/main/java/ru/javawebinar/topjava/LoggedUser.java
@@ -9,7 +9,7 @@ import ru.javawebinar.topjava.util.UserMealsUtil;
 public class LoggedUser {
 
     public static int id() {
-        return 1;
+        return 2;
     }
 
     public static int getCaloriesPerDay() {
